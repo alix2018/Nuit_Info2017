f6713d9bd4dce857bff80f226c135b1976160547
Thibaut Buchert <thibaut.buchert@gmail.com>
2016-12-02 00:10:28 +0100

    Ajout modèles

---
+++ db/migrate/20161201223945_create_associations.rb
@@ -0,0 +1,10 @@
+class CreateAssociations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :associations do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201215931) do
+ActiveRecord::Schema.define(version: 20161201225755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "contents"
+    t.boolean  "accepted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "associations", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "title"
+    t.text     "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
